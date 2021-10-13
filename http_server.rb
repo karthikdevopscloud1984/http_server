f65743cc9cfc34e64604015fbd2cfc144405cf6f
karthik <karthikdevopscloud1984@gmail.com>
2021-10-13 10:43:56 +0530

    changes to the ruby script to get response with http1.1

--- http_server.rb
+++ http_server.rb
@@ -1,20 +1,25 @@
 require 'socket'
 
-server  = TCPServer.new('0.0.0.0', 80)
+server = TCPServer.new('0.0.0.0', 80)
 
-loop {
-  client  = server.accept
-  request = client.readpartial(2048)
+while client = server.accept
+  request = client.gets
   
   method, path, version = request.lines[0].split
 
-  puts "#{method} #{path} #{version}"
+  puts request
 
   if path == "/healthcheck"
+    client.print "HTTP/1.1 200\r\n"
+    client.print "Content-Type: text/html\r\n"
+    client.print "\r\n"
     client.write("OK")
   else
+    client.print "HTTP/1.1 200\r\n"
+    client.print "Content-Type: text/html\r\n"
+    client.print "\r\n"
     client.write("Well, hello there!")
   end
 
   client.close
-}
+end
